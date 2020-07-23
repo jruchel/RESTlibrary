80fd9e42c75555d91e90e15309f4af5bb3ac28a3
kcuf <freddyk2651@gmail.com>
2020-07-23 16:01:22 +0200

    no message

--- src/main/java/org/whatever/library/configuration/WebSecurityConfig.java
+++ src/main/java/org/whatever/library/configuration/WebSecurityConfig.java
@@ -32,11 +32,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/admin/**", "/users/**", "/users").hasRole("ADMIN")
                 .antMatchers(HttpMethod.POST, "/authors", "authors/{id}/**", "authors/{id}").hasRole("MODERATOR")
                 .antMatchers(HttpMethod.GET, "/authors/**", "/authors").hasRole("USER")
-<<<<<<< HEAD
                 .antMatchers("/registration", "/error", "/test/**", "/logger").permitAll()
-=======
-                .antMatchers("/registration", "/error", "/test/**", "/temp/login").permitAll()
->>>>>>> master
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
